a7e48c8d8494fda9d0b6a478ad96f35d58b52287
GabrielMBoeira <gboeira777@gmail.com>
2022-11-29 21:40:33 -0300

    Autenticação laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,31 @@
 <?php
 
-use App\Http\Controllers\Admin\CommentController;
-use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::put('/users/{id}/comments', [CommentController::class, 'update'])->name('comments.update');
-Route::get('/users/{id}/comments/create', [CommentController::class, 'create'])->name('comments.create');
-Route::get('/users/{user}/comments/{id}', [CommentController::class, 'edit'])->name('comments.edit');
-Route::put('/comments/{id}', [CommentController::class, 'update'])->name('comments.update');
-Route::post('/users/{id}/comments', [CommentController::class, 'store'])->name('comments.store');
-Route::get('/users/{id}/comments', [CommentController::class, 'index'])->name('comments.index');
-
-Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('users.destroy');
-Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
-Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
-Route::get('/users', [UserController::class, 'index'])->name('users.index');
-Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-Route::post('/users', [UserController::class, 'store'])->name('users.store');
-
-Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
-
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
